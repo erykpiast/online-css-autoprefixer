a9a99dae1fc3a1776ac6074ad52045ab7f5e1653
Eryk Napierała <eryk.piast@gmail.com>
2015-02-21 12:24:19 +0100

    fix test-dev plan

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('build', function() {
 gulp.task('test:lint', require('./gulp/test/lint'));
 gulp.task('test:build', require('./gulp/test/build'));
 gulp.task('test:run', require('./gulp/test/run'));
-gulp.task('_test', function(cb) {
+gulp.task('test', function(cb) {
     runSequence(
         [ 'lint', 'test:lint' ],
         'test:build',
@@ -35,10 +35,10 @@ gulp.task('_test', function(cb) {
         }
     );
 });
-gulp.task('test', function() {
+gulp.task('test-dev', function() {
     // concatenating order matters, config.src.js.files has negative matching for test files
-    gulp.watch(config.src.js.files.concat(config.test.files), [ '_test' ]);
-    gulp.start('_test');
+    gulp.watch(config.src.js.files.concat(config.test.files), [ 'test' ]);
+    gulp.start('test');
 });
 
 gulp.task('default', [ 'lint', 'build', 'webserver' ]);

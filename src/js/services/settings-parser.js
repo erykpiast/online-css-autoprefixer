331d13a5458cfc6ebf15031182759586ad77fcee
Eryk Napierała <eryk.piast@gmail.com>
2015-02-21 11:31:30 +0000

    try to add new parse function for settings parser

--- src/js/services/settings-parser.js
+++ src/js/services/settings-parser.js
@@ -5,7 +5,7 @@ import browserslist from 'browserslist';
 import compareVersions from 'version-compare.js';
 
 
-// @function parse - parse browserlist pattern to the list of browsers
+// @function parse - convert browserlist pattern to the list of browsers
 // @access public
 // @param {string} string - browserlist pattern to parse
 // @return {object} parsed browserlist pattern
@@ -27,7 +27,7 @@ export function parse (string) {
 }
 
 
-/* @function stringify - convert settings object to Autoprefixer configuration string
+/* @function stringify - convert settings object to browserlist pattern
  * @access public
  * @param {object} settings - settings object
  *     @property {object} [settings.popularity = undefined] - popularity matcher
@@ -95,4 +95,101 @@ export function stringify (settings) {
     });
 
     return autoprefixerConfig.join(',');
+}
+
+
+/* @function destringify - convert Autoprefixer configuration string to settings object
+ * @access public
+ * @param {string} autoprefixerConfig - browserlist pattern
+ * @returns {object} settings - settings object
+ *     @property {object} [settings.popularity = undefined] - popularity matcher
+ *         @property {number} settings.popularity[country | 'global'] - popularity for country or entire world
+ *     @property {object} [settings.lastVersions = undefined] - last versions matcher
+ *         @property {number} settings.lastVersions[browser | 'all'] - number of last version for specific browser or for all browsers
+ *     @property {array} [settings.versionComparison = undefined] - older than or never than matchers
+ *         @property {object} settings.versionComparison[browser] - version comparison entities for the browser
+ *             @property {object} [settings.versionComparison[browser].newerThan = undefined] - newer than matcher
+ *                 @property {boolean} settings.versionComparison[browser].newerThan.equal - true if greater or equal
+ *                 @property {string} settings.versionComparison[browser].newerThan.version - version to compare with
+ *             @property {object} [settings.versionComparison[browser].olderThan = undefined] - older than matcher
+ *                 @property {boolean} settings.versionComparison[browser].olderThan.equal - true if lower or equal
+ *                 @property {string} settings.versionComparison[browser].olderThan.version - version to compare with
+ *     @property {array} [settings.newerThan = undefined] - newer (or equal) than matcher
+ *         @property {number} settings.newerThan[browser] - browser chosen by newerThan matcher
+ *     @property {array} [settings.direct = undefined] - direct matcher
+ *         @property {number} settings.direct[browser] - browser chosen by direct matcher (includes ESR matcher)
+ */
+export function destringify (autoprefixerConfig) {
+    var settings = { };
+    
+    autoprefixerConfig
+        .split(',')
+        .map((part) => part.trim())
+        .forEach(function(part) {
+            each(browserlist.queries, function(query, queryName) {
+                var match = part.match(query.regexp);
+                
+                if(!match) {
+                    return;
+                }
+                
+                match = match.slice(1);
+                
+                switch(queryName) {
+                    case 'globalStatistics':
+                    case 'countryStatistics':
+                        if(!settings.hasOwnProperty('popularity')) {
+                            settings.popularity = { };
+                        }
+                    case 'globalStatistics':
+                        settings.popularity.global = match[0];
+                    break;
+                    case 'countryStatistics':
+                        settings.popularity[match[1]] = match[0];
+                    break;
+                    case 'lastVersions':
+                    case 'lastByBrowser':
+                        if(!settings.hasOwnProperty('lastVersions')) {
+                            settings.lastVersions = { };
+                        }
+                    case 'lastVersions':
+                        settings.lastVersions.all = match[0];
+                    break;
+                    case 'lastByBrowser':
+                        settings.lastVersions[match[1]] = match[0];
+                    break;
+                    case 'versions':
+                        if(!settings.hasOwnProperty('versionComparison')) {
+                            settings.versionComparison = { };
+                        }
+                        
+                        if(!settings.versionComparison.hasOwnProperty(match[0])) {
+                            settings.versionComparison[match[0]] = { };
+                        }
+                        
+                        let browser = settings.versionComparison[match[0]];
+                        
+                        if(match[1] === '<') {
+                            browser.olderThan = {
+                                version: match[2],
+                                equal: match[1].indexOf('=') !== -1
+                            };
+                        } else if(match[1] === '>') {
+                            browser.newerThan = {
+                                version: match[2],
+                                equal: match[1].indexOf('=') !== -1
+                            };
+                        }
+                    break;
+                    case 'esr':
+                        
+                    break;
+                    case 'direct':
+                        
+                    break;
+                }
+            });
+        });
+
+    return settings;
 }
